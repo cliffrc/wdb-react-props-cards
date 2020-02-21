00baf789e6abf5d7d6a422f05be9b8ecc8a5c794
Cliff Cunnington <cliff@10strata.com>
2020-02-21 06:24:26 +0100

    Add two cards
    added Cards for superbuds Spiderman and Jay Pritchett.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,18 @@ ReactDOM.render(
       tel="+123 456 789"
       email="b@beyonce.com"
     />
+    <Card
+      name="Spiderman"
+      img="https://i.pinimg.com/236x/3b/01/d8/3b01d8f7c2de09ff3f1b86ed5ac1d693.jpg"
+      tel="555-super-spider"
+      email="spiderman@marvel.com"
+    />
+    <Card
+      name="Jay Pritchett"
+      img="http://www.usanetwork.com/sites/usanetwork/files/styles/300x250/public/modernfamily_gallery_jay-1.jpg?itok=k6z_oTe5"
+      tel="+1 415 555 5555"
+      email="jp@modernfamily.com"
+    />
   </div>,
   document.getElementById("root")
 );
